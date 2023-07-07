4c006a1fc60eaf2779840a51c04f6928b3dc4121
Rahad Arman Nabid <rahadarmannabid@Rahads-MacBook-Air.local>
2023-07-07 10:19:55 -0400

    commit base template done

--- app.py
+++ app.py
@@ -1,30 +1,61 @@
-from flask import Flask, render_template, request, jsonify
-from prompts_looping import printing
+#these are the libraries we are going to use in building our app
+from flask import Flask, render_template, request, jsonify, Response, render_template_string
+from prompts_looping import printing, get_completion, str2dict
 
+# Initialize the Flask application
 app = Flask(__name__)
+#global variables
+increment = -1  #this is the serial number of the question that will be asked to chatgpt
+html_code = '' # this is the html file
+data = '' # this is the data associated with the html file
 
+
+# Define a route for the default URL, which loads the form, this is the initial page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+#process the data from the form and send it to the progress page
 @app.route('/process', methods=['POST'])
 def process():
+    global html_code, data
     html_code = request.form['paragraph1']
     data = request.form['paragraph2']
-    # printing(html_code, data)
-    # Do something with the paragraphs
-    return render_template('progress.html')
+    return render_template("progress.html", chart=html_code, data=data)
 
 
-@app.route('/progress', methods=['POST'])
-def update_progress():
-    data = request.get_json()
-    progress = data.get('progress')
+#this is the page where the chatbot will be displayed
+@app.route('/send_data', methods=['POST'])
+def send_number():
+    global increment #initialize the global variable increment
+    number = int(request.form['value']) #get the value from the frontend which is always 1
+    increment = number + increment # we will increment each value by 1 to iterate all the questions one by one
+    print('increment', increment) #print the value of increment
+    response, prompt_base = printing(html_code, data, increment) #call the printing function to get the response from chatgpt, response is the output from chatgpt in dictionary format
+    
+    if isinstance(response, str): #if the response is a string then we will convert it into dictionary
+        print("The variable is a string.") #print the value of string
 
-    # Do something with the progress value
-    print('Progress Update:', progress)
+        #we will convert the response into dictionary format using the get completion and str2dict function
+        prompt = f"""
+        Convert this response into python dictionary format. Do not use other information than python dictionary.
+        The response is 
+        ```{response}```
+        """
+        response = get_completion(prompt)
+        response = str2dict(response)
+        # again we will check if the response is a string or dictonary
+        if isinstance(response, str):
+            response = {"error": "The response is not a string."} #if not we will print the error
+        else:
+            pass #otherwise we will pass
+    else:
+        print('hello', type(response))
+    # response = {'response': "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.(This is a dummy response"}
+    
+    
+    return render_template('progress.html', response=response, chart=html_code, data=data, prompt_base = prompt_base)
 
-    return jsonify({'message': 'Progress updated.'})
 
 if __name__ == '__main__':
     app.run(debug=True)
--- prompts_looping.py
+++ prompts_looping.py
@@ -19,8 +19,8 @@ import time
 import ast
 
 
-#gpt-4.0
-openai.api_key = "sk-FMyijawHFRnfjE9IazAzT3BlbkFJX0fSTRWHl3XLD32B7z67000"
+#gpt-3.5
+openai.api_key = 
 
 
 
@@ -34,16 +34,41 @@ def get_completion(prompt, model='gpt-4'):
     return response.choices[0].message["content"]
 
 
-def printing(html_code, data):
+def printing(html_code, data, number):
     ques = pd.read_csv("prompts_scatterplot.csv")
     a , b = ques.shape
-    for i in range(0, a):
-        prompt_base = str(ques.iloc[i][3]) + str(ques.iloc[i][4])
-        prompt = f""" ```{prompt_base}```
-        The html code is inside this backtick.
-        ```{html_code}```
-        The data associate with it is inside this backtick.
-        ```{data}```
-        """
-        response = get_completion(prompt)
-        print(response)
+    prompt_base = str(ques.iloc[number][3]) + str(ques.iloc[number][4])
+    prompt = f""" ```{prompt_base}```
+    The html code is inside this backtick.
+    ```{html_code}```
+    The data associate with it is inside this backtick.
+    ```{data}```
+    """
+    response = get_completion(prompt)
+    response = str2dict(response)
+  
+    return response, prompt_base
+
+
+def str2dict(string_data):
+    start = []
+    stop = []
+    for index, char in enumerate(string_data):
+        if char == "{":
+            start.append(index)
+        if char == "}":
+            stop.append(index)
+      # Character not found
+
+    try:
+        a = start[0]
+        b = stop[-1]
+        dict_str = string_data[a : b + 1]
+        dict_str = dict_str.replace("\'", "\"")
+        print(dict_str)
+        nested_dict = json.loads(dict_str)
+        print(type(nested_dict))
+        return nested_dict
+    except:
+
+        return string_data
